801f297098d2484436ada2e8af658ae6606f2046
Michal Zbranek <blixer27@gmail.com>
2014-10-12 23:58:15 +0200

    GhostDB

--- HauntedHauses/src/test/java/com/mycompany/hauntedhauses/dao/jpa/ResidentDAOImplTest.java
+++ HauntedHauses/src/test/java/com/mycompany/hauntedhauses/dao/jpa/ResidentDAOImplTest.java
@@ -35,7 +35,7 @@ public class ResidentDAOImplTest {
     @BeforeClass
     public static void setup() {
         try {
-            emf = Persistence.createEntityManagerFactory("ResidentDB");
+            emf = Persistence.createEntityManagerFactory("GhostDB");
         } catch (Exception e) {
             e.printStackTrace();
             fail("Could not initialize Persistence.");
