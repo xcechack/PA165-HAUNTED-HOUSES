378fa5efc29b0bd0c2b915e1131e013822134f83
Jana Cechackova <jana.cechackova@gmail.com>
2014-10-13 03:58:55 +0200

    funkcni testy pro PowerDAOImpl pridany

--- HauntedHauses/src/test/java/com/mycompany/hauntedhauses/dao/jpa/PowerDAOImplTest.java
+++ HauntedHauses/src/test/java/com/mycompany/hauntedhauses/dao/jpa/PowerDAOImplTest.java
@@ -5,13 +5,7 @@
  */
 package com.mycompany.hauntedhauses.dao.jpa;
 
-import com.mycompany.hauntedhauses.entity.Ghost;
-import com.mycompany.hauntedhauses.entity.House;
 import com.mycompany.hauntedhauses.entity.Power;
-import com.mycompany.hauntedhauses.entity.field.Address;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -29,10 +23,17 @@ import org.junit.Test;
 public class PowerDAOImplTest {
     private static EntityManagerFactory emf;
     private static EntityManager em;
-    private static Ghost ghost1;
-    private static House house;
-    private static Power power;
+    private static PowerDAOImpl powerManager;
     
+    private static Power power1;
+    private static Power power2;
+  
+    
+     
+    public PowerDAOImplTest(){
+        powerManager = new PowerDAOImpl(emf);
+        clearPowersFromDatabase(); 
+    }
     
     @BeforeClass
     public static void setup() {
@@ -42,101 +43,150 @@ public class PowerDAOImplTest {
             e.printStackTrace();
             fail("Could not initialize Persistence.");
         }
-        em = emf.createEntityManager();
-        em.getTransaction().begin();
+//        em = emf.createEntityManager();
+//        em.getTransaction().begin();
+              
         
-        ghost1 = new Ghost();
         
-        ghost1.setName("MyGhost");
-        long time = System.currentTimeMillis();
-        Timestamp startTime = new Timestamp(time);
-        Timestamp endTime = new Timestamp(time+3600000);
-        ghost1.setStartTime(startTime);
-        ghost1.setEndTime(endTime);
-        ghost1.setInfo("SomeInfoAboutMyGhost");
-        
-        house = new House();
-        house.setName("MyHouse");
-        Address address = new Address();
-        address.setStreet("MyAdress");
-        address.setHouseNumber(15);
-        address.setCity("MyTown");
-        address.setPostalCode(12345);
-        house.setAddress(address);
-        Date date = new Date(time);
-        house.setHauntedFrom(date);
-        house.setHistory("Was haunted before");
-        em.persist(house);
-        
-        ghost1.setHouse(house);
-        
-        power = new Power();
-        power.setName("Fire");
-        power.setDescription("Mortal");
-        em.persist(power);
-        
-        List powers = new ArrayList<Power>();
-        powers.add(power);
-        ghost1.setPowers(powers);
-        
-        em.persist(ghost1);
-        try {
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail("Could not commit Persistence.");
-        }
-	em.close();
+//        ghost1 = new Ghost();
+//        
+//        ghost1.setName("MyGhost");
+//        long time = System.currentTimeMillis();
+//        Timestamp startTime = new Timestamp(time);
+//        Timestamp endTime = new Timestamp(time+3600000);
+//        ghost1.setStartTime(startTime);
+//        ghost1.setEndTime(endTime);
+//        ghost1.setInfo("SomeInfoAboutMyGhost");
+//        
+//        house = new House();
+//        house.setName("MyHouse");
+//        Address address = new Address();
+//        address.setStreet("MyAdress");
+//        address.setHouseNumber(15);
+//        address.setCity("MyTown");
+//        address.setPostalCode(12345);
+//        house.setAddress(address);
+//        Date date = new Date(time);
+//        house.setHauntedFrom(date);
+//        house.setHistory("Was haunted before");
+//        em.persist(house);
+//        
+//        ghost1.setHouse(house);
+//        
+//        power = new Power();
+//        power.setName("Fire");
+//        power.setDescription("Mortal");
+//        em.persist(power);
+//        
+//        List powers = new ArrayList<Power>();
+//        powers.add(power);
+//        ghost1.setPowers(powers);
+//        
+//        em.persist(ghost1);
+//        try {
+//            em.getTransaction().commit();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            fail("Could not commit Persistence.");
+//        }
+//	em.close();
     }
     
     @AfterClass
     public static void tearDown() {
-        PowerDAOImpl powerManager = new PowerDAOImpl(emf);
-        em = emf.createEntityManager();
-        House toBeRemoved = em.merge(house);
-        em.remove(toBeRemoved);
-        Power toBeRemovedPower = em.merge(power);
-        em.remove(toBeRemovedPower);
-        powerManager.deletePower(power);
+//        PowerDAOImpl powerManager = new PowerDAOImpl(emf);
+//        em = emf.createEntityManager();
+//        House toBeRemoved = em.merge(house);
+//        em.remove(toBeRemoved);
+//        Power toBeRemovedPower = em.merge(power);
+//        em.remove(toBeRemovedPower);
+//        powerManager.deletePower(power);
     }
 
     @Test
     public void testAddPower() {
-        PowerDAOImpl powerManager = new PowerDAOImpl(emf);
-        Power power2 = powerManager.getPowerByID(ghost1.getId());
-        System.out.println("Power 1: " + power.toString());
-        System.out.println("Power 2: " + power2.toString());
-        Assert.assertTrue(power.equals(power2));
-    }
+        power1 = new Power();  
+        power1.setName("Fire");
+        power1.setDescription("xxx");
+        
+        List<Power> powers = powerManager.getAllPowers();
+        Assert.assertEquals(0, powers.size());        
+        
+        powerManager.addPower(power1);
+        powers = powerManager.getAllPowers();
+        Assert.assertEquals(1, powers.size());   
+        
+        powerManager.deletePower(power1);
+        
+        }
     
     @Test
     public void testUpdatePower() {
-        PowerDAOImpl powerManager = new PowerDAOImpl(emf);
-        power.setName("mySecondPower");
-        powerManager.updatePower(power);
-        Power power2 = powerManager.getPowerByID(power.getId());
-        Assert.assertTrue(power.equals(power2));
+       power1 = new Power();  
+       power1.setName("Fire");
+       power1.setDescription("xxx"); 
+      
+       powerManager.addPower(power1);
+       power1.setName("FireFire");
+       powerManager.updatePower(power1);
+       
+       String newNameTest = powerManager.getPowerByID(power1.getId()).getName();
+       Assert.assertEquals(newNameTest, "FireFire");
+       
+       powerManager.deletePower(power1);            
     }
     
     @Test
     public void testDeletePower() {
-        PowerDAOImpl powerManager = new PowerDAOImpl(emf);
-        powerManager.deletePower(power);
-        Assert.assertNull(powerManager.getPowerByID(power.getId()));
-        powerManager.addPower(power);
+        power1 = new Power();  
+        power1.setName("Fire");
+        power1.setDescription("xxx");
+        
+        powerManager.addPower(power1);
+        Assert.assertEquals(powerManager.getAllPowers().size(), 1);
+        
+        powerManager.deletePower(power1);
+        Assert.assertEquals(powerManager.getAllPowers().size(), 0);
+        
     }
     
     @Test
     public void testGetAllPowers() {
-        PowerDAOImpl powerManager = new PowerDAOImpl(emf);
-	List<Power> powers = powerManager.getAllPowers();
-	Assert.assertEquals(1, powers.size());
+        power1 = new Power();
+        power1.setName("Fire");
+        power1.setDescription("xxx");
+        powerManager.addPower(power1);
+        Assert.assertEquals(powerManager.getAllPowers().size(), 1);
+        
+        power2 = new Power();
+        power2.setName("Storm");
+        power2.setDescription("xxx");
+        powerManager.addPower(power2);
+        Assert.assertEquals(powerManager.getAllPowers().size(), 2);
+        
+        clearPowersFromDatabase();
     }
     
     @Test
     public void testGetPowerById() {
-        PowerDAOImpl powerManager = new PowerDAOImpl(emf);
-        Power power2 = powerManager.getPowerByID(power.getId());
-        Assert.assertTrue(power.equals(power2));
+        power1.setName("Fire");
+        power1.setDescription("xxx");
+        powerManager.addPower(power1);
+        
+        long id = power1.getId();
+        Power power3 = powerManager.getPowerByID(id);
+        Assert.assertEquals(power1, power3);
+        
+        powerManager.deletePower(power1);
+    }
+    
+   
+    public void clearPowersFromDatabase(){
+        
+        List<Power> powers = powerManager.getAllPowers();
+        for (int i = 0; i<powers.size();i++){
+            //System.out.println("Power to delete: "+powers.get(i).getId()+"\n");
+            powerManager.deletePower(powers.get(i));
+        }
     }
 }
