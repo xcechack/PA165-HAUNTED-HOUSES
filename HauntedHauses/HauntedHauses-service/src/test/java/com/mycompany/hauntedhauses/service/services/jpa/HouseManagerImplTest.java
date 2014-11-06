366304a96071fcd7d52046fba8c2386eab27792c
GabiP <gabriela.podolnikova@gmail.com>
2014-11-06 21:21:36 +0100

    test package

---
+++ HauntedHauses/HauntedHauses-service/src/test/java/com/mycompany/hauntedhauses/service/services/jpa/HouseManagerImplTest.java
@@ -0,0 +1,87 @@
+package com.mycompany.hauntedhauses.service.services.jpa;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import com.mycompany.hauntedhauses.dao.HouseDAO;
+import com.mycompany.hauntedhauses.entity.House;
+import com.mycompany.hauntedhauses.service.dto.HouseDTO;
+import com.mycompany.hauntedhauses.service.services.HouseManager;
+import org.dozer.Mapper;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author Gabriela Podolnikova
+ */
+public class HouseManagerImplTest {
+    
+    @Mock
+    private HouseDAO houseDAO;
+    
+    private HouseManager houseManager;
+    
+    @Autowired
+    private Mapper mapper;
+    
+    public HouseManagerImplTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+    @Test
+    public void addHouse() {
+    
+    }
+    
+    @Test
+    public void deleteHouse() {
+        /*HouseDTO houseDTO = new HouseDTO();
+        
+        houseManager.addHouse(houseDTO);
+        Mockito.verify(houseDAO).addHouse(mapper.map(houseDTO, House.class));
+        
+        houseManager.deleteHouse(houseDTO);
+        Mockito.verify(houseDAO).deleteHouse(mapper.map(houseDTO, House.class));*/
+    }
+    
+    @Test
+    public void updateHouse() {
+        
+    }
+    
+    @Test
+    public void getAllHouses() {
+        
+    }
+    
+    @Test
+    public void getHouseById() {
+        
+    }
+}
