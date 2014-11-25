5d2aa0e615914fb4f6ea1618f9ecf3ccb06a35a8
GabiP <gabriela.podolnikova@gmail.com>
2014-11-25 15:55:23 +0100

    house service test

--- HauntedHouses/HauntedHouses-dao/target/generated-sources/annotations/cz/muni/fi/pa165/hauntedhouses/entity/Ghost_.java
+++ HauntedHouses/HauntedHouses-dao/target/generated-sources/annotations/cz/muni/fi/pa165/hauntedhouses/entity/Ghost_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-25T14:54:01")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-25T15:50:23")
 @StaticMetamodel(Ghost.class)
 public class Ghost_ { 
 
---
+++ HauntedHouses/HauntedHouses-service/src/test/java/cz/muni/fi/pa165/hauntedhouses/service/HouseManagerTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.hauntedhouses.service;
+
+import cz.muni.fi.pa165.hauntedhouses.dao.HouseDAO;
+import cz.muni.fi.pa165.hauntedhouses.entity.House;
+import cz.muni.fi.pa165.hauntedhouses.service.dto.HouseDTO;
+import cz.muni.fi.pa165.hauntedhouses.service.services.HouseManager;
+import cz.muni.fi.pa165.hauntedhouses.service.services.jpa.HouseManagerImpl;
+import org.dozer.DozerBeanMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import org.mockito.MockitoAnnotations;
+
+/**
+ *
+ * @author Gabriela Podolnikova
+ */
+public class HouseManagerTest {
+    private DozerBeanMapper dozerBeanMapper;
+    private HouseManager houseManager; 
+    private HouseDTO houseDTO;
+    private House house;
+    
+    @Mock
+    HouseDAO houseDAO;
+    
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        houseManager = new HouseManagerImpl();
+        dozerBeanMapper = new DozerBeanMapper();
+        houseDTO = new HouseDTO();
+        houseDTO.setId(1l);
+        houseDTO.setHistory("some history");
+        houseDTO.setName("some name");
+        houseManager.setHouseDAO(houseDAO);
+        house = dozerBeanMapper.map(houseDTO, House.class);
+    }
+    
+    @Test
+    public void testAddHouse(){
+        houseManager.addHouse(houseDTO);
+        verify(houseDAO).addHouse(house);
+        houseManager.deleteHouse(houseDTO);
+    }
+    
+    @Test
+    public void testUpdateHouse(){
+        houseManager.addHouse(houseDTO);
+        houseDTO.setId(2l);
+        house = dozerBeanMapper.map(houseDTO, House.class);
+        houseManager.updateHouse(houseDTO);
+        verify(houseDAO).updateHouse(house);
+        houseManager.deleteHouse(houseDTO);
+    }
+    
+    @Test
+    public void testDeleteHouse(){
+        houseManager.addHouse(houseDTO);
+        houseManager.deleteHouse(houseDTO);
+        verify(houseDAO).deleteHouse(house);        
+    }
+    
+    @Test
+    public void getAllHouses(){
+        houseManager.addHouse(houseDTO);
+        houseManager.getAllHouses();
+        verify(houseDAO).getAllHouses();
+    }
+    
+    @Test
+    public void getHouseById(){
+        houseManager.addHouse(houseDTO);
+        houseManager.getHouseById(1l);
+        verify(houseDAO).getHouseById(1l);
+    }
+}
