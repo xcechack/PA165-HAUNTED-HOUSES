5328b5f579838a5601b0f9180a8964dbf335cc71
Janca <jana.cechackova@gmail.com>
2014-11-30 13:18:28 +0100

    all appContext modified, main page added

---
+++ HauntedHouses/HauntedHouses-web/src/main/java/cz/muni/fi/pa165/hauntedhouses/config/SpringMvcConfig.java
@@ -0,0 +1,82 @@
+package cz.muni.fi.pa165.hauntedhouses.config;
+
+//import cz.muni.fi.pa165.books.BookService;
+//import cz.muni.fi.pa165.books.jpa.BookServiceJPAImpl;
+import cz.muni.fi.pa165.hauntedhouses.service.services.GhostManager;
+import cz.muni.fi.pa165.hauntedhouses.service.services.jpa.GhostManagerImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+/**
+ * The central Spring context and Spring MVC configuration.
+ * The @Configuration annotation declares it as Spring configuration.
+ * The @EnableWebMvc enables default  MVC config for using @Controller, @RequestMapping and so on,
+ * see http://docs.spring.io/spring/docs/current/spring-framework-reference/html/mvc.html#mvc-config-enable
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "cz.muni.fi.pa165")
+@ImportResource({"classpath:applicationContext-web.xml"})
+public class SpringMvcConfig extends WebMvcConfigurerAdapter {
+
+    final static Logger log = LoggerFactory.getLogger(SpringMvcConfig.class);
+
+    /**
+     * Maps the main page to a specific view.
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        log.debug("mapping URL / to home view");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/snoop").setViewName("snoop");
+    }
+
+
+    /**
+     * Enables default Tomcat servlet that serves static files.
+     */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        log.debug("enabling default servlet for static files");
+        configurer.enable();
+    }
+
+    /**
+     * Provides mapping from view names to JSP pages in WEB-INF/jsp directory.
+     */
+    @Bean
+    public ViewResolver viewResolver() {
+        log.debug("registering JSP in /WEB-INF/jsp/ as views");
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    /**
+     * Provides localized messages.
+     */
+    @Bean
+    public MessageSource messageSource() {
+        log.debug("registering ResourceBundle 'Texts' for messages");
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("Texts");
+        return messageSource;
+    }
+
+
+}
